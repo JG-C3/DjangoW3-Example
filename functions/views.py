3c9801d02ea61eaf166cf2a7c139bd32768a1be7
JG-C3 <jgc3django@gmail.com>
2022-09-28 19:08:13 +0900

    Update Django W3

--- functions/views.py
+++ functions/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from random import *
-import datetime
 
 # Create your views here.
 def index(request):
@@ -10,56 +8,10 @@ def image(request):
     return render(request, "image.html")
 
 def lucky(request):
-    name = "규리"
-    number = randint(1, 100)
-    context = {
-        "name": name,
-        "number": number,
-    }
-
-    return render(request, "lucky.html", context)
+    return render(request, "lucky.html")
 
 def birthday(request):
-    today = datetime.date.today()
-    year = today.year
-    month = today.month
-    day = today.day
-
-    b_month = 1
-    b_day = 1
-
-    if (b_month > month):
-        b_year = year
-    elif (b_month == month) and (b_day >= day):
-        b_year = year
-    else:
-        b_year = year + 1
-
-    birthday = datetime.date(b_year, b_month, b_day)
-
-    d_day = birthday - today
-    d_day = d_day.days
-
-    context = {
-        "year": year,
-        "month": month,
-        "day": day,
-        "b_month": b_month,
-        "b_day": b_day,
-        "d_day": d_day,
-    }
-
-    return render(request, "birthday.html", context)
+    return render(request, "birthday.html")
 
 def table(request):
-    contents = [
-        "불고기",
-        "파스타",
-        "칼국수",
-        "치킨",
-        "비빔밥",
-    ]
-    context = {
-        "contents": contents,
-    }
-    return render(request, "table.html", context)
+    return render(request, "table.html")
